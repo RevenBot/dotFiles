868e9e9742fa147fb5c426d0c62da2ff0db3beb5
Kevin Sinchi <kevinjss97@gmail.com>
2026-06-26 22:36:18 +0200

    :sparkles: Change nvim custom conf to nvchad conf

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,29 +1,6 @@
---global options
-local o = vim.opt
---buffer-local options
-local bo = vim.bo
---window-local options
-local wo = vim.wo
+require "nvchad.options"
 
--- for newtree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- add yours here!
 
-
---global options
-o.syntax = 'on'
-o.smartcase = true
-o.tabstop = 4
-o.softtabstop = 4
-o.shiftwidth = 4 
-o.termguicolors = true
-o.encoding="utf-8"
-o.mouse = ""
-
--- buffer-local options
-bo.autoindent = true 
-
-
--- window-local options
-wo.relativenumber = true 
-
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
--- .config/nvim/lua/plugins/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-local nvimtree= require("nvim-tree")
-nvimtree.setup()
-vim.keymap.set('n', '<leader>n', ':NvimTreeFindFileToggle<CR>')
